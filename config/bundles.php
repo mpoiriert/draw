7d8db72ed706cb38dd6d0e6ad18d3af498ec991a
Martin Poirier-Theoret <mpoiriert@gmail.com>
2019-10-21 14:40:46 -0400

    install sonata admin (not configured)

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,12 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Draw\Bundle\TesterBundle\DrawTesterBundle::class => ['test' => true],
+    Sonata\DatagridBundle\SonataDatagridBundle::class => ['dev' => true, 'test' => true],
+    Sonata\CoreBundle\SonataCoreBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Sonata\BlockBundle\SonataBlockBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
+    Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['dev' => true, 'test' => true],
 ];
