d0a208f3335e7cacecaf9d5555fceb3cd35170d9
Martin Poirier-Theoret <mpoiriert@gmail.com>
2019-11-15 20:10:18 -0500

    violation list tester

---
+++ src/Component/Tester/Data/ViolationListTester.php
@@ -0,0 +1,59 @@
+<?php namespace Breethe\TestHelper;
+
+use Draw\Component\Tester\DataTester;
+
+class ViolationListTester
+{
+    private $violations;
+
+    public function __invoke(DataTester $tester)
+    {
+        $tester->assertCount(
+            count($this->violations),
+            "Current violations:\n" . json_encode($tester->getData(), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)
+        );
+
+        foreach ($this->violations as $index => $violation) {
+            foreach ($violation as $property => $value) {
+                $tester->path("[$index].$property")->assertSame($value);
+            }
+        }
+    }
+
+    /**
+     * @param $propertyPath
+     * @param $message
+     * @return $this
+     */
+    public function addViolation($propertyPath, $message)
+    {
+        $this->violations[] = compact('propertyPath', 'message');
+        return $this;
+    }
+
+    /**
+     * Check code of the last added violation
+     *
+     * @param $code
+     * @return $this
+     */
+    public function code($code)
+    {
+        $this->violations[count($this->violations) - 1]['code'] = $code;
+
+        return $this;
+    }
+
+    /**
+     * Check invalid value on the last added violation
+     *
+     * @param $invalidValue
+     * @return $this
+     */
+    public function invalidValue($invalidValue)
+    {
+        $this->violations[count($this->violations) - 1]['invalidValue'] = $invalidValue;
+
+        return $this;
+    }
+}
