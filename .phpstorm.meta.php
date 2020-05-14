a30c93f45d01e3d66aa7099aada3a174d9f6cf78
Martin Poirier-Theoret <mpoiriert@gmail.com>
2020-05-14 19:10:11 -0400

    phpstorm meta php file

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -1,23 +1,7 @@
 <?php namespace PHPSTORM_META {
     $STATIC_METHOD_TYPES = [
-        \Prophecy\Prophecy\ObjectProphecy::reveal('') => [
-            "" == "@",
-        ],
-        \Draw\Component\Tester\ServiceTesterTrait::getService('') => [
-            "" == "@",
-        ],
         \Symfony\Contracts\EventDispatcher\EventDispatcherInterface::dispatch('') => [
             "" == "@"
         ]
     ];
-}
-
-namespace Prophecy\Prophecy {
-    class ObjectProphecy
-    {
-        public function reveal(string $class = null)
-        {
-
-        }
-    }
 }
---
+++ src/Component/Tester/.phpstorm.meta.php
@@ -0,0 +1,7 @@
+<?php namespace PHPSTORM_META {
+    $STATIC_METHOD_TYPES = [
+        \Draw\Component\Tester\ServiceTesterTrait::getService('') => [
+            "" == "@",
+        ],
+    ];
+}
