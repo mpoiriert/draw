7fe4cfa9b1d6f65ecc25dd9526dd27264d813482
Martin Poirier-Theoret <mpoiriert@gmail.com>
2020-05-20 16:03:57 -0400

    open link notification

---
+++ src/Bundle/DashboardBundle/Feedback/OpenLink.php
@@ -0,0 +1,30 @@
+<?php namespace Draw\Bundle\DashboardBundle\Feedback;
+
+use Draw\Bundle\DashboardBundle\Client\FeedbackInterface;
+use JsonSerializable;
+
+class OpenLink implements FeedbackInterface, JsonSerializable
+{
+    private $url;
+
+    private $target;
+
+    public function __construct(string $url, string $target = '_blank')
+    {
+        $this->url = $url;
+        $this->target = $target;
+    }
+
+    public function getFeedbackType(): string
+    {
+        return 'open-link';
+    }
+
+    public function jsonSerialize()
+    {
+        return [
+            'url' => $this->url,
+            'target' => $this->target
+        ];
+    }
+}
