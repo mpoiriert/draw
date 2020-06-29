0c1d80ff656333d8975c5e003f42cb6ea45294ba
Martin Poirier-Theoret <mpoiriert@gmail.com>
2020-06-29 12:59:52 -0400

    confirm yes/no label translation

--- src/Bundle/DashboardBundle/Annotations/ConfirmFlow.php
+++ src/Bundle/DashboardBundle/Annotations/ConfirmFlow.php
@@ -13,11 +13,17 @@ class ConfirmFlow extends Flow
 
     private $message = '_flow.confirm.message';
 
+    private $yesLabel = '_flow.confirm.yes';
+
+    private $noLabel = '_flow.confirm.no';
+
     public function __construct(array $values = [])
     {
         $values = array_merge(
             [
                 'message' => $this->message,
+                'yesLabel' => $this->yesLabel,
+                'noLabel' => $this->noLabel
             ],
             $values
         );
@@ -43,5 +49,25 @@ class ConfirmFlow extends Flow
     public function setMessage($message): void
     {
         $this->message = Translatable::set($this->message, $message);
+    }
+
+    public function getYesLabel(): string
+    {
+        return $this->yesLabel;
+    }
+
+    public function setYesLabel($yesLabel): void
+    {
+        $this->yesLabel = Translatable::set($this->yesLabel, $yesLabel);
+    }
+
+    public function getNoLabel(): string
+    {
+        return $this->noLabel;
+    }
+
+    public function setNoLabel($noLabel): void
+    {
+        $this->noLabel = Translatable::set($this->noLabel, $noLabel);
     }
 }
