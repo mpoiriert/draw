3e7328d3a9f63ab760e52de5c61268fda8ed04de
Martin Poirier-Theoret <mpoiriert@gmail.com>
2020-08-16 12:11:19 -0400

    Test bundle getService use is own kernel

--- src/Bundle/TesterBundle/DependencyInjection/ServiceTesterTrait.php
+++ src/Bundle/TesterBundle/DependencyInjection/ServiceTesterTrait.php
@@ -3,14 +3,41 @@
 namespace Draw\Bundle\TesterBundle\DependencyInjection;
 
 use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Contracts\Service\ResetInterface;
 
 /**
  * @method static KernelInterface bootKernel(array $options = [])
  */
 trait ServiceTesterTrait
 {
+    private static $mainTestKernel = null;
+    private static $mainTestContainer = null;
+
     public static function getService($service)
     {
-        return static::bootKernel()->getContainer()->get('test.service_container')->get($service);
+        if (null === self::$mainTestContainer) {
+            self::$mainTestKernel = static::createKernel();
+            self::$mainTestKernel->boot();
+            $container = self::$mainTestKernel->getContainer();
+            self::$mainTestContainer = $container->has('test.service_container') ? $container->get('test.service_container') : $container;
+        }
+
+        return self::$mainTestContainer->get($service);
+    }
+
+    /**
+     * @afterClass
+     */
+    public static function shutdownMainTestContainer()
+    {
+        if (null !== self::$mainTestKernel) {
+            self::$mainTestKernel->shutdown();
+        }
+
+        if (self::$mainTestContainer instanceof ResetInterface) {
+            self::$mainTestContainer->reset();
+        }
+
+        self::$mainTestKernel = self::$mainTestContainer = null;
     }
 }
