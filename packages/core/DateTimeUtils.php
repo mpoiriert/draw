c983d5092918b6087273c8a9b4db8e840ad5f445
Martin Poirier Theoret <mpoiriert@gmail.com>
2022-02-19 10:00:53 -0500

    [Core] DateTimeUtils

---
+++ packages/core/DateTimeUtils.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Draw\Component\Core;
+
+use DateTime;
+use DateTimeImmutable;
+use DateTimeInterface;
+
+final class DateTimeUtils
+{
+    private function __construct()
+    {
+    }
+
+    public static function isSameTimestamp(?DateTimeInterface $dateTime1, ?DateTimeInterface $dateTime2): bool
+    {
+        switch (true) {
+            case $dateTime1 === $dateTime2:
+                return true;
+            case null === $dateTime1:
+            case null === $dateTime2:
+                return false;
+            default:
+                return $dateTime1->getTimestamp() === $dateTime2->getTimestamp();
+        }
+    }
+
+    public static function toDateTimeImmutable(?DateTimeInterface $dateTime): ?DateTimeImmutable
+    {
+        switch (true) {
+            case null === $dateTime:
+                return null;
+            default:
+                return DateTimeImmutable::createFromFormat('U', $dateTime->getTimestamp());
+        }
+    }
+
+    public static function toDateTime(?DateTimeInterface $dateTime): ?DateTime
+    {
+        switch (true) {
+            case null === $dateTime:
+                return null;
+            default:
+                return DateTime::createFromFormat('U', $dateTime->getTimestamp());
+        }
+    }
+}
