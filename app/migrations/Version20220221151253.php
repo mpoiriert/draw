5800611699ab44f2f10932afaa5acbbe6cd66816
Martin Poirier Theoret <mpoiriert@gmail.com>
2022-02-21 12:36:19 -0500

    [UserBundle] UserLock cannot have unique key since doctrine delete after insert

--- app/migrations/Version20220221151253.php
+++ app/migrations/Version20220221151253.php
@@ -21,13 +21,11 @@ final class Version20220221151253 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE draw_user__user_lock CHANGE id id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\'');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_A86CF708A76ED3953BB8880C ON draw_user__user_lock (user_id, reason)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP INDEX UNIQ_A86CF708A76ED3953BB8880C ON draw_user__user_lock');
         $this->addSql('ALTER TABLE draw_user__user_lock CHANGE id id INT AUTO_INCREMENT NOT NULL');
     }
 }
