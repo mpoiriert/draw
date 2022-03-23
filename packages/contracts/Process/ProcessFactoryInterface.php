4336316d4b0f951849d43e77d0092e56c0e607dc
Martin Poirier Theoret <mpoiriert@gmail.com>
2022-03-23 10:12:18 -0400

    [Contracts] Process Factory

---
+++ packages/contracts/Process/ProcessFactoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Draw\Contracts\Process;
+
+use Symfony\Component\Process\Process;
+
+interface ProcessFactoryInterface
+{
+    public function create(array $command, string $cwd = null, array $env = null, $input = null, ?float $timeout = 60): Process;
+}
