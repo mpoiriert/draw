30fa7b1a1ec006078160fc881bae87b53a56a60b
Martin Poirier Theoret <mpoiriert@gmail.com>
2022-03-23 10:12:40 -0400

    [Process] Process Factory

---
+++ packages/process/ProcessFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Draw\Component\Process;
+
+use Draw\Contracts\Process\ProcessFactoryInterface;
+use Symfony\Component\Process\Process;
+
+/**
+ * This class allow to injection to create a process.
+ */
+class ProcessFactory implements ProcessFactoryInterface
+{
+    public function create(array $command, string $cwd = null, array $env = null, $input = null, ?float $timeout = 60): Process
+    {
+        return new Process(...func_get_args());
+    }
+}
---
+++ packages/process/Tests/ProcessFactoryTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Draw\Component\Process\Tests;
+
+use Draw\Component\Process\ProcessFactory;
+use Draw\Contracts\Process\ProcessFactoryInterface;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Process\Process;
+
+class ProcessFactoryTest extends TestCase
+{
+    private $service;
+
+    public function setUp(): void
+    {
+        $this->service = new ProcessFactory();
+    }
+
+    public function testConstruct(): void
+    {
+        $this->assertInstanceOf(
+            ProcessFactoryInterface::class,
+            $this->service
+        );
+    }
+
+    public function testCreateDefault(): void
+    {
+        $process = $this->service->create(['cd']);
+
+        $this->assertInstanceOf(Process::class, $process);
+
+        $this->assertSame(
+            "'cd'",
+            $process->getCommandLine()
+        );
+
+        $this->assertSame(
+            getcwd(),
+            $process->getWorkingDirectory()
+        );
+
+        $this->assertNull(
+            $process->getEnv()
+        );
+
+        $this->assertNull(
+            $process->getInput()
+        );
+
+        $this->assertSame(
+            60.0,
+            $process->getTimeout()
+        );
+    }
+
+    public function testCreateWithArguments(): void
+    {
+        $process = $this->service->create(
+            ['cd'],
+            $workingDirectory = __DIR__,
+            $env = ['key' => 'value'],
+            $input = 'input',
+            $timeout = 5.0
+        );
+
+        $this->assertInstanceOf(Process::class, $process);
+
+        $this->assertSame(
+            "'cd'",
+            $process->getCommandLine()
+        );
+
+        $this->assertSame(
+            $workingDirectory,
+            $process->getWorkingDirectory()
+        );
+
+        $this->assertSame(
+            $env,
+            $process->getEnv()
+        );
+
+        $this->assertSame(
+            $input,
+            $process->getInput()
+        );
+
+        $this->assertSame(
+            $timeout,
+            $process->getTimeout()
+        );
+    }
+}
