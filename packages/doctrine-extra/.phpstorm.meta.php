e1aec6e9c092c67ee44e82c16c826da96c6a0051
Martin Poirier Theoret <mpoiriert@gmail.com>
2022-04-02 18:56:25 -0400

    [DoctrineExtra] add ObjectRferenceTrait

---
+++ packages/doctrine-extra/.phpstorm.meta.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PHPSTORM_META;
+
+/**
+ * @noinspection PhpUnusedLocalVariableInspection
+ * @noinspection PhpIllegalArrayKeyTypeInspection
+ */
+$STATIC_METHOD_TYPES = [
+    \Draw\DoctrineExtra\Common\DataFixtures\ObjectReferenceTrait::getObjectReference('') => [
+        '' === '@',
+    ],
+];
