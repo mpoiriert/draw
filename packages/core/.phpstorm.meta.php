4a6456b1a646f0a252351ab5d7622b958d368cf6
Martin Poirier Theoret <mpoiriert@gmail.com>
2022-04-04 21:37:45 -0400

    [Core] provide phpstorm.meta.php

---
+++ packages/core/.phpstorm.meta.php
@@ -0,0 +1,8 @@
+<?php namespace PHPSTORM_META {
+
+    $STATIC_METHOD_TYPES = [
+        \Symfony\Component\Security\Http\Authenticator\Passport\Passport::getBadge() => [
+            "" == "@"
+        ]
+    ];
+}
