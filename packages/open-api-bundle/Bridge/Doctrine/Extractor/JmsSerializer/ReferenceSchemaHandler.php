e73b935f78df5dc03acedccac774ab92dcca749e
Martin Poirier Theoret <mpoiriert@gmail.com>
2022-04-05 14:56:38 -0400

    [OpenApiBundle] fix object reference type

--- packages/open-api-bundle/Bridge/Doctrine/Extractor/JmsSerializer/ReferenceSchemaHandler.php
+++ packages/open-api-bundle/Bridge/Doctrine/Extractor/JmsSerializer/ReferenceSchemaHandler.php
@@ -5,8 +5,10 @@ namespace Draw\Bundle\OpenApiBundle\Bridge\Doctrine\Extractor\JmsSerializer;
 use Doctrine\Persistence\ManagerRegistry;
 use Draw\Component\OpenApi\Extraction\ExtractionContextInterface;
 use Draw\Component\OpenApi\Extraction\Extractor\JmsSerializer\TypeHandler\TypeToSchemaHandlerInterface;
+use Draw\Component\OpenApi\Extraction\Extractor\TypeSchemaExtractor;
 use Draw\Component\OpenApi\Schema\Schema;
 use JMS\Serializer\Metadata\PropertyMetadata;
+use Metadata\MetadataFactoryInterface;
 
 class ReferenceSchemaHandler implements TypeToSchemaHandlerInterface
 {
@@ -15,9 +17,15 @@ class ReferenceSchemaHandler implements TypeToSchemaHandlerInterface
      */
     private $managerRegistry;
 
-    public function __construct(ManagerRegistry $managerRegistry)
+    /**
+     * @var MetadataFactoryInterface
+     */
+    private $metadataFactory;
+
+    public function __construct(ManagerRegistry $managerRegistry, MetadataFactoryInterface $factory)
     {
         $this->managerRegistry = $managerRegistry;
+        $this->metadataFactory = $factory;
     }
 
     public function extractSchemaFromType(
@@ -29,12 +37,13 @@ class ReferenceSchemaHandler implements TypeToSchemaHandlerInterface
         }
 
         $propertySchema = new Schema();
-        $propertySchema->type = $type;
+        $propertySchema->type = $type['type'];
+        $propertySchema->format = $type['format'] ?? null;
 
         return $propertySchema;
     }
 
-    private function getReferenceType(PropertyMetadata $item)
+    private function getReferenceType(PropertyMetadata $item): ?array
     {
         switch (true) {
             case !isset($item->type['name']):
@@ -44,10 +53,27 @@ class ReferenceSchemaHandler implements TypeToSchemaHandlerInterface
         }
 
         $class = $item->type['params'][0]['name'];
+
         $metadataFor = $this->managerRegistry->getManagerForClass($class)
             ->getMetadataFactory()
             ->getMetadataFor($class);
 
-        return $metadataFor->getTypeOfField($metadataFor->getIdentifierFieldNames()[0]);
+        return $this->getTypeFromJsmFactory(
+            $class,
+            $metadataFor->getIdentifierFieldNames()[0]
+        );
+    }
+
+    private function getTypeFromJsmFactory(string $class, string $propertyId): ?array
+    {
+        switch (true) {
+            case null === $classMetadata = $this->metadataFactory->getMetadataForClass($class):
+            case null === $propertyMetadata = $classMetadata->propertyMetadata[$propertyId] ?? null:
+                return null;
+        }
+
+        $type = $propertyMetadata->type['name'];
+
+        return TypeSchemaExtractor::getPrimitiveType($type);
     }
 }
