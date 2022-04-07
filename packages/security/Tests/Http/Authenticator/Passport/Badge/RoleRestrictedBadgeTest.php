e217c78e516d51af0ffc07cbf4e33f271ddc3386
Martin Poirier Theoret <mpoiriert@gmail.com>
2022-04-07 16:29:47 -0400

    [Security] automation test

---
+++ packages/security/Tests/Http/Authenticator/Passport/Badge/RoleRestrictedBadgeTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Draw\Component\Security\Tests\Http\Authenticator\Passport\Badge;
+
+use Draw\Component\Security\Http\Authenticator\Passport\Badge\RoleRestrictedBadge;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Draw\Component\Security\Http\Authenticator\Passport\Badge\RoleRestrictedBadge
+ */
+class RoleRestrictedBadgeTest extends TestCase
+{
+    private RoleRestrictedBadge $entity;
+
+    private string $role;
+
+    public function setUp(): void
+    {
+        $this->entity = new RoleRestrictedBadge($this->role = uniqid('ROLE_'));
+    }
+
+    public function testGetRole(): void
+    {
+        $this->assertSame(
+            $this->role,
+            $this->entity->getRole()
+        );
+    }
+
+    public function testIsResolved(): void
+    {
+        $this->assertFalse($this->entity->isResolved());
+    }
+
+    public function testMarkResolved(): void
+    {
+        $this->entity->markResolved();
+
+        $this->assertTrue($this->entity->isResolved());
+    }
+}
---
+++ packages/security/Tests/Jwt/JwtEncoderTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Draw\Component\Security\Tests\Jwt;
+
+use DateTimeImmutable;
+use Draw\Component\Security\Jwt\JwtEncoder;
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\SignatureInvalidException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Draw\Component\Security\Jwt\JwtEncoder
+ */
+class JwtEncoderTest extends TestCase
+{
+    private JwtEncoder $service;
+
+    public function setUp(): void
+    {
+        $this->service = new JwtEncoder(
+            uniqid('key-'),
+            'HS256'
+        );
+    }
+
+    public function testEncode(): void
+    {
+        $token = $this->service->encode(['value' => $value = uniqid('value-')]);
+
+        $this->assertSame(
+            $value,
+            $this->service->decode($token)->value
+        );
+    }
+
+    public function testDecodeExpired(): void
+    {
+        $token = $this->service->encode(
+            ['value' => uniqid('value-')],
+            new DateTimeImmutable('- 10 minutes')
+        );
+
+        $this->expectException(ExpiredException::class);
+
+        $this->service->decode($token);
+    }
+
+    public function testDecodeWrongKey(): void
+    {
+        $service = new JwtEncoder(uniqid('key-'), 'HS256');
+
+        $token = $this->service->encode(
+            ['value' => uniqid('value-')],
+            new DateTimeImmutable('- 10 minutes')
+        );
+
+        $this->expectException(SignatureInvalidException::class);
+
+        $service->decode($token);
+    }
+}
