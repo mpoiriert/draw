6952862271d5c51589c6c0b8f515d727ab550b36
Martin Poirier Theoret <mpoiriert@gmail.com>
2022-05-14 15:38:55 -0400

    [Messenger] BaseEnvelopeFactory now dispatch EnvelopeCreatedEvent

--- packages/messenger/EnvelopeFactory/BasicEnvelopeFactory.php
+++ packages/messenger/EnvelopeFactory/BasicEnvelopeFactory.php
@@ -3,15 +3,26 @@
 namespace Draw\Component\Messenger\EnvelopeFactory;
 
 use Draw\Component\Messenger\Entity\MessageHolderInterface;
+use Draw\Component\Messenger\Event\EnvelopeCreatedEvent;
 use Symfony\Component\Messenger\Envelope;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 class BasicEnvelopeFactory implements EnvelopeFactoryInterface
 {
-    public function createEnvelopes(MessageHolderInterface $object, array $messages): array
+    private EventDispatcherInterface $eventDispatcher;
+
+    public function __construct(EventDispatcherInterface $eventDispatcher)
+    {
+        $this->eventDispatcher = $eventDispatcher;
+    }
+
+    public function createEnvelopes(MessageHolderInterface $messageHolder, array $messages): array
     {
         $envelopes = [];
         foreach ($messages as $message) {
-            $envelopes[] = new Envelope($message);
+            $envelopes[] = $this->eventDispatcher
+                ->dispatch(new EnvelopeCreatedEvent($messageHolder, new Envelope($message)))
+                ->getEnvelope();
         }
 
         return $envelopes;
---
+++ packages/messenger/Event/EnvelopeCreatedEvent.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Draw\Component\Messenger\Event;
+
+use Draw\Component\Messenger\Entity\MessageHolderInterface;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Contracts\EventDispatcher\Event;
+
+class EnvelopeCreatedEvent extends Event
+{
+    private Envelope $envelope;
+
+    private MessageHolderInterface $messageHolder;
+
+    public function __construct(MessageHolderInterface $messageHolder, Envelope $envelope)
+    {
+        $this->envelope = $envelope;
+        $this->messageHolder = $messageHolder;
+    }
+
+    public function getMessageHolder(): MessageHolderInterface
+    {
+        return $this->messageHolder;
+    }
+
+    public function getEnvelope(): Envelope
+    {
+        return $this->envelope;
+    }
+
+    public function setEnvelope(Envelope $envelope): void
+    {
+        $this->envelope = $envelope;
+    }
+}
