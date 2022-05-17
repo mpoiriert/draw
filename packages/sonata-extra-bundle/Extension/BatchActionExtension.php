ea9c12dd8d47fc1b76b35c24026dd482e257e8a4
Martin Poirier Theoret <mpoiriert@gmail.com>
2022-05-17 10:21:16 -0400

    [SonataExtraBundle] Batch action

---
+++ packages/sonata-extra-bundle/Extension/BatchActionExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Draw\Bundle\SonataExtraBundle\Extension;
+
+use Doctrine\Inflector\InflectorFactory;
+use Draw\Bundle\SonataExtraBundle\Controller\BatchController;
+use Sonata\AdminBundle\Admin\AbstractAdminExtension;
+use Sonata\AdminBundle\Admin\AdminInterface;
+use Sonata\AdminBundle\Route\RouteCollectionInterface;
+
+class BatchActionExtension extends AbstractAdminExtension
+{
+    public function configureRoutes(AdminInterface $admin, RouteCollectionInterface $collection): void
+    {
+        if (!$collection->has('batch')) {
+            return;
+        }
+
+        $collection
+            ->get('batch')
+            ->setDefault('_controller', BatchController::class.'::batchAction');
+    }
+
+    public function configureBatchActions(AdminInterface $admin, array $actions): array
+    {
+        foreach ($actions as $name => $configuration) {
+            if (isset($configuration['controller'])) {
+                continue;
+            }
+
+            $actions[$name]['controller'] = sprintf(
+                '%s::batchAction%s',
+                $admin->getBaseControllerName(),
+                InflectorFactory::create()->build()->classify($name)
+            );
+        }
+
+        return $actions;
+    }
+}
