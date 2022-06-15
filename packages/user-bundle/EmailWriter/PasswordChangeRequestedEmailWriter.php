b6b608d4210f79fb5f46bfca3f9ecd4f5ba2848e
Martin Poirier Théorêt <mpoiriert@gmail.com>
2022-06-15 15:23:01 -0400

    [UserBundle] track last password change to prevent password change request to change it twice

--- packages/user-bundle/EmailWriter/PasswordChangeRequestedEmailWriter.php
+++ packages/user-bundle/EmailWriter/PasswordChangeRequestedEmailWriter.php
@@ -4,22 +4,29 @@ namespace Draw\Bundle\UserBundle\EmailWriter;
 
 use DateTimeImmutable;
 use Draw\Bundle\UserBundle\Email\PasswordChangeRequestedEmail;
+use Draw\Bundle\UserBundle\Entity\SecurityUserInterface;
 use Draw\Bundle\UserBundle\Message\RedirectToSecuredRouteMessage;
 use Draw\Component\Mailer\EmailWriter\EmailWriterInterface;
 use Draw\Component\Messenger\ManualTrigger\ManuallyTriggeredMessageUrlGenerator;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 class PasswordChangeRequestedEmailWriter implements EmailWriterInterface
 {
     private ManuallyTriggeredMessageUrlGenerator $messageUrlGenerator;
+
+    private UserProviderInterface $userProvider;
 
     public static function getForEmails(): array
     {
         return ['compose' => 0];
     }
 
-    public function __construct(ManuallyTriggeredMessageUrlGenerator $messageUrlGenerator)
-    {
+    public function __construct(
+        ManuallyTriggeredMessageUrlGenerator $messageUrlGenerator,
+        UserProviderInterface $userProvider
+    ) {
         $this->messageUrlGenerator = $messageUrlGenerator;
+        $this->userProvider = $userProvider;
     }
 
     public function compose(PasswordChangeRequestedEmail $email): void
@@ -29,11 +36,19 @@ class PasswordChangeRequestedEmailWriter implements EmailWriterInterface
         }
 
         if (!$email->getCallToActionLink()) {
+            $user = $this->userProvider->loadUserByIdentifier($email->getUserIdentifier());
+
+            $parameters = [];
+            if ($user instanceof SecurityUserInterface) {
+                $parameters['t'] = $user->getPasswordUpdatedAt() ? $user->getPasswordUpdatedAt()->getTimestamp() : 0;
+            }
+
             $email->callToActionLink(
                 $this->messageUrlGenerator->generateLink(
                     new RedirectToSecuredRouteMessage(
                         $email->getUserIdentifier(),
-                        'admin_change_password'
+                        'admin_change_password',
+                        $parameters
                     ),
                     new DateTimeImmutable('+ 1 day'),
                     'change_password'
