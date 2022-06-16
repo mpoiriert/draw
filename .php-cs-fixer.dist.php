939f8ce2b3447df53085ab020cdd5e28f78837fa
Martin Poirier Théorêt <mpoiriert@gmail.com>
2022-06-16 18:50:22 -0400

    [internal] php-cs-fixer phpdoc_order

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -11,6 +11,7 @@ return (new \PhpCsFixer\Config())
         '@Symfony:risky' => true,
         'array_syntax' => ['syntax' => 'short'],
         'php_unit_test_case_static_method_calls' => true,
+        'phpdoc_order' => true
     ])
     ->setFinder($finder)
 ;
