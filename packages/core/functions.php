5ccafd4a527f89a78f5cbd2dde5364ba020d16d1
Martin Poirier Théorêt <mpoiriert@gmail.com>
2022-09-18 18:16:10 -0400

    [Core] use_trait check also trait

--- packages/core/functions.php
+++ packages/core/functions.php
@@ -7,12 +7,22 @@ if (!\function_exists(__NAMESPACE__.'\use_trait')) {
     {
         $class = $objectOrClass;
 
+        $allTraits = [];
         do {
-            $traits = class_uses($class, true);
+            $traits = class_uses($class);
             if (isset($traits[$trait])) {
                 return true;
             }
+
+            $allTraits = array_merge($allTraits, $traits);
         } while ($class = get_parent_class($class));
+
+        foreach (array_unique($allTraits) as $usedTrait) {
+            $traits = class_uses($usedTrait);
+            if (isset($traits[$trait])) {
+                return true;
+            }
+        }
 
         return false;
     }
