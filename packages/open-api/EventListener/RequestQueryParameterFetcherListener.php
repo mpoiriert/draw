65ea6f4cdc1b78dc0af2fd3b6fee278127f8065c
Martin Poirier Théorêt <mpoiriert@gmail.com>
2023-05-13 07:50:41 -0400

    [OpenApi] QueryParameter better extraction

--- packages/open-api/EventListener/RequestQueryParameterFetcherListener.php
+++ packages/open-api/EventListener/RequestQueryParameterFetcherListener.php
@@ -35,73 +35,61 @@ class RequestQueryParameterFetcherListener implements EventSubscriberInterface
             return;
         }
 
-        $parameters = [];
+        $queryParameters = QueryParameter::fromReflectionMethod(
+            new \ReflectionMethod(\get_class($controllerObject), $controller[1])
+        );
 
-        $reflectionParameters = (new \ReflectionMethod(\get_class($controllerObject), $controller[1]))
-            ->getParameters();
+        foreach ($queryParameters as $queryParameter) {
+            $name = $queryParameter->name;
 
-        foreach ($reflectionParameters as $reflectionParameter) {
-            $attributes = $reflectionParameter
-                ->getAttributes(QueryParameter::class, \ReflectionAttribute::IS_INSTANCEOF);
+            if ($request->attributes->has($name) && null !== $request->attributes->get($name)) {
+                throw new \InvalidArgumentException(sprintf(
+                    'QueryParameterFetcherSubscriber parameter conflicts with a path parameter [%s] for route [%s]',
+                    $name,
+                    $request->attributes->get('_route')
+                ));
+            }
 
-            foreach ($attributes as $attribute) {
-                $attribute = $attribute->newInstance();
-
-                \assert($attribute instanceof QueryParameter);
-
-                $parameters[] = $attribute;
-
-                $name = $attribute->name ??= $reflectionParameter->getName();
-
-                if ($request->attributes->has($name) && null !== $request->attributes->get($name)) {
-                    throw new \InvalidArgumentException(sprintf(
-                        'QueryParameterFetcherSubscriber parameter conflicts with a path parameter [%s] for route [%s]',
-                        $name,
-                        $request->attributes->get('_route')
-                    ));
+            if ($request->query->has($name)) {
+                switch ($queryParameter->type) {
+                    case 'string':
+                        $value = (string) $request->query->get($name);
+                        break;
+                    case 'integer':
+                        $value = $request->query->getInt($name);
+                        break;
+                    case 'boolean':
+                        $value = $request->query->getBoolean($name);
+                        break;
+                    case 'number':
+                        /** @var @phpstan-ignore-next-line $value */
+                        $value = $request->query->get($name) + 0;
+                        break;
+                    case 'array':
+                        $separator = match ($queryParameter->collectionFormat) {
+                            'csv' => ',',
+                            'ssv' => ' ',
+                            'tsv' => "\t",
+                            'pipes' => '|',
+                            // no break
+                            default => throw new \RuntimeException(sprintf(
+                                'Unsupported collection format [%s]',
+                                $queryParameter->collectionFormat
+                            )),
+                        };
+                        $value = explode($separator, (string) $request->query->get($name));
+                        break;
+                    default:
+                        $value = $request->query->get($name);
+                        break;
                 }
 
-                if ($request->query->has($name)) {
-                    switch ($attribute->type) {
-                        case 'string':
-                            $value = (string) $request->query->get($name);
-                            break;
-                        case 'integer':
-                            $value = $request->query->getInt($name);
-                            break;
-                        case 'boolean':
-                            $value = $request->query->getBoolean($name);
-                            break;
-                        case 'number':
-                            /** @var @phpstan-ignore-next-line $value */
-                            $value = $request->query->get($name) + 0;
-                            break;
-                        case 'array':
-                            $separator = match ($attribute->collectionFormat) {
-                                'csv' => ',',
-                                'ssv' => ' ',
-                                'tsv' => "\t",
-                                'pipes' => '|',
-                                // no break
-                                default => throw new \RuntimeException(sprintf(
-                                    'Unsupported collection format [%s]',
-                                    $attribute->collectionFormat
-                                )),
-                            };
-                            $value = explode($separator, (string) $request->query->get($name));
-                            break;
-                        default:
-                            $value = $request->query->get($name);
-                            break;
-                    }
-
-                    $request->attributes->set($name, $value);
-                }
+                $request->attributes->set($name, $value);
             }
         }
 
-        if ($parameters) {
-            $request->attributes->set('_draw_query_parameters_validation', $parameters);
+        if ($queryParameters) {
+            $request->attributes->set('_draw_query_parameters_validation', $queryParameters);
         }
     }
 }
