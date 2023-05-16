2241c70f149b1815ef67e5620bc99c28d6dcda7b
Martin Poirier Théorêt <mpoiriert@gmail.com>
2023-05-16 16:59:16 -0400

    [Application] Application sepcific exception for configuration and version

--- packages/contracts/Application/ConfigurationRegistryInterface.php
+++ packages/contracts/Application/ConfigurationRegistryInterface.php
@@ -2,10 +2,14 @@
 
 namespace Draw\Contracts\Application;
 
+use Draw\Contracts\Application\Exception\ConfigurationIsNotAccessibleException;
+
 interface ConfigurationRegistryInterface
 {
     /**
      * Return the configuration value or default if it doesn't exist.
+     *
+     * @throws ConfigurationIsNotAccessibleException
      */
     public function get(string $name, mixed $default = null);
 
@@ -13,16 +17,22 @@ interface ConfigurationRegistryInterface
      * Set the configuration value.
      *
      * The value is expected to be json_encoded
+     *
+     * @throws ConfigurationIsNotAccessibleException
      */
     public function set(string $name, mixed $value): void;
 
     /**
      * Check if the configuration base on the name exists or not.
+     *
+     * @throws ConfigurationIsNotAccessibleException
      */
     public function has(string $name): bool;
 
     /**
      * Delete the configuration base on its name.
+     *
+     * @throws ConfigurationIsNotAccessibleException
      */
     public function delete(string $name): void;
 }
--- packages/contracts/Application/VersionVerificationInterface.php
+++ packages/contracts/Application/VersionVerificationInterface.php
@@ -2,11 +2,22 @@
 
 namespace Draw\Contracts\Application;
 
+use Draw\Contracts\Application\Exception\VersionInformationIsNotAccessibleException;
+
 interface VersionVerificationInterface
 {
+    /**
+     * @throws VersionInformationIsNotAccessibleException
+     */
     public function getRunningVersion(): ?string;
 
+    /**
+     * @throws VersionInformationIsNotAccessibleException
+     */
     public function getDeployedVersion(): ?string;
 
+    /**
+     * @throws VersionInformationIsNotAccessibleException
+     */
     public function isUpToDate(): bool;
 }
