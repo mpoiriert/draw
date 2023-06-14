b1e302ba7356727cf6fda786a7bc849b1209c735
Martin Poirier Théorêt <mpoiriert@gmail.com>
2023-06-14 08:13:26 -0400

    [Tester] CommandDataTester expected error output support same type as expected display

--- packages/tester/Application/CommandDataTester.php
+++ packages/tester/Application/CommandDataTester.php
@@ -7,50 +7,45 @@ use Symfony\Component\Console\Command\Command;
 
 final class CommandDataTester
 {
-    private ?string $expectedDisplay = null;
-
-    private array $expectedDisplayStrings = [];
+    private null|string|array $expectedDisplay = null;
+    private null|string|array $expectedErrorOutput = null;
 
     private int $expectedStatusCode = 0;
 
-    private ?string $errorOutput = '';
-
     /**
-     * @param string|array|null $expectedDisplay String for exact match, array to find line, null will not test the display
+     * @param string|array|null $expectedDisplay     String for exact match, array to find line, null will not test the display
+     * @param string|array|null $expectedErrorOutput String for exact match, array to find line, null will not test the error output
      */
-    public static function create(int $expectedStatusCode = Command::SUCCESS, $expectedDisplay = ''): self
-    {
-        $self = (new self())->setExpectedStatusCode($expectedStatusCode);
-
-        if (\is_array($expectedDisplay)) {
-            $self->setExpectedDisplayStrings($expectedDisplay);
-        } else {
-            $self->setExpectedDisplay($expectedDisplay);
-        }
-
-        return $self;
+    public static function create(
+        int $expectedStatusCode = Command::SUCCESS,
+        null|string|array $expectedDisplay = '',
+        null|string|array $expectedErrorOutput = '',
+    ): self {
+        return (new self())->setExpectedStatusCode($expectedStatusCode)
+            ->setExpectedDisplay($expectedDisplay)
+            ->setExpectedErrorOutput($expectedErrorOutput);
     }
 
-    public function getExpectedDisplayStrings(): array
-    {
-        return $this->expectedDisplayStrings;
-    }
-
-    public function setExpectedDisplayStrings(array $strings): self
-    {
-        $this->expectedDisplayStrings = $strings;
-
-        return $this;
-    }
-
-    public function getExpectedDisplay(): ?string
+    public function getExpectedDisplay(): array|string|null
     {
         return $this->expectedDisplay;
     }
 
-    public function setExpectedDisplay(?string $expectedDisplay): self
+    public function setExpectedDisplay(array|string|null $expectedDisplay): self
     {
         $this->expectedDisplay = $expectedDisplay;
+
+        return $this;
+    }
+
+    public function getExpectedErrorOutput(): array|string|null
+    {
+        return $this->expectedErrorOutput;
+    }
+
+    public function setExpectedErrorOutput(array|string|null $expectedErrorOutput): self
+    {
+        $this->expectedErrorOutput = $expectedErrorOutput;
 
         return $this;
     }
@@ -67,32 +62,28 @@ final class CommandDataTester
         return $this;
     }
 
-    public function getErrorOutput(): ?string
-    {
-        return $this->errorOutput;
-    }
-
-    public function setErrorOutput(?string $errorOutput): self
-    {
-        $this->errorOutput = $errorOutput;
-
-        return $this;
-    }
-
     public function __invoke(DataTester $dataTester): void
     {
         $dataTester->path('statusCode')->assertEquals($this->expectedStatusCode);
 
-        if (null !== $this->errorOutput) {
-            $dataTester->path('errorOutput')->assertEquals($this->errorOutput);
+        if (null !== $this->expectedErrorOutput) {
+            if (\is_string($this->expectedErrorOutput)) {
+                $dataTester->path('errorOutput')->assertEquals($this->expectedErrorOutput);
+            } else {
+                foreach ($this->expectedErrorOutput as $expectedErrorOutput) {
+                    $dataTester->path('errorOutput')->assertStringContainsString($expectedErrorOutput);
+                }
+            }
         }
 
         if (null !== $this->expectedDisplay) {
-            $dataTester->path('display')->assertEquals($this->expectedDisplay);
-        }
-
-        foreach ($this->expectedDisplayStrings as $expectedDisplayString) {
-            $dataTester->path('display')->assertStringContainsString($expectedDisplayString);
+            if (\is_string($this->expectedDisplay)) {
+                $dataTester->path('display')->assertEquals($this->expectedDisplay);
+            } else {
+                foreach ($this->expectedDisplay as $expectedDisplay) {
+                    $dataTester->path('display')->assertStringContainsString($expectedDisplay);
+                }
+            }
         }
     }
 }
