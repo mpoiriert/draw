0f7ac30f1ddacefce387af1da796f7938eddd86a
Martin Poirier Théorêt <mpoiriert@gmail.com>
2023-10-31 19:54:45 -0400

    [FrameworkExtraBundle] Entity Migrator compiler pass check service exists

--- packages/framework-extra-bundle/DependencyInjection/Compiler/EntityMigratorCompilerPass.php
+++ packages/framework-extra-bundle/DependencyInjection/Compiler/EntityMigratorCompilerPass.php
@@ -3,15 +3,23 @@
 namespace Draw\Bundle\FrameworkExtraBundle\DependencyInjection\Compiler;
 
 use Draw\Component\EntityMigrator\MigrationInterface;
+use Draw\Component\EntityMigrator\Migrator;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\Compiler\ServiceLocatorTagPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
 use Symfony\Component\DependencyInjection\Reference;
 
 class EntityMigratorCompilerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container): void
     {
+        try {
+            $container->findDefinition(Migrator::class);
+        } catch (ServiceNotFoundException) {
+            return;
+        }
+
         $references = [];
         foreach (array_keys($container->findTaggedServiceIds(MigrationInterface::class)) as $id) {
             $name = $container
