9cc9c00f21f089bcd574577d531100cf7b2412af
Martin Poirier Théorêt <mpoiriert@gmail.com>
2023-11-01 14:36:33 -0400

    [SonataIntegrationBundle/EntityMigrator] Better admin integration

--- packages/sonata-integration-bundle/EntityMigrator/Admin/BaseEntityMigrationAdmin.php
+++ packages/sonata-integration-bundle/EntityMigrator/Admin/BaseEntityMigrationAdmin.php
@@ -2,10 +2,16 @@
 
 namespace Draw\Bundle\SonataIntegrationBundle\EntityMigrator\Admin;
 
+use Draw\Component\EntityMigrator\Entity\BaseEntityMigration;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\Type\ModelAutocompleteType;
 use Sonata\AdminBundle\Route\RouteCollectionInterface;
 use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\DoctrineORMAdminBundle\Filter\ChoiceFilter;
+use Sonata\DoctrineORMAdminBundle\Filter\ModelFilter;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 abstract class BaseEntityMigrationAdmin extends AbstractAdmin
 {
@@ -15,6 +21,38 @@ abstract class BaseEntityMigrationAdmin extends AbstractAdmin
         'group' => 'Entity Migrator',
         'icon' => 'fas fa-cogs',
     ];
+
+    protected function configureDatagridFilters(DatagridMapper $filter): void
+    {
+        $filter
+            ->add('id')
+            ->add(
+                'migration',
+                ModelFilter::class,
+                [
+                    'field_type' => ModelAutocompleteType::class,
+                    'field_options' => [
+                        'property' => 'name',
+                    ],
+                    'show_filter' => true,
+                ]
+            )
+            ->add(
+                'state',
+                ChoiceFilter::class,
+                [
+                    'field_type' => ChoiceType::class,
+                    'field_options' => [
+                        'multiple' => true,
+                        'choices' => array_combine(
+                            BaseEntityMigration::STATES,
+                            BaseEntityMigration::STATES
+                        ),
+                    ],
+                    'show_filter' => true,
+                ]
+            );
+    }
 
     protected function configureListFields(ListMapper $list): void
     {
@@ -44,6 +82,6 @@ abstract class BaseEntityMigrationAdmin extends AbstractAdmin
     protected function configureRoutes(RouteCollectionInterface $collection): void
     {
         $collection
-            ->clearExcept('list');
+            ->clearExcept(['list', 'show']);
     }
 }
