b067aee7b4222ff7fa28e03344fdafdc0a50214d
Martin Poirier Théorêt <mpoiriert@gmail.com>
2023-11-13 08:50:56 -0500

    [Console] Filter in/out support

---
+++ packages/framework-extra-bundle/Console/EventListener/DocumentationFilterCommandEventListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Draw\Bundle\FrameworkExtraBundle\Console\EventListener;
+
+use Draw\Component\Console\Event\GenerateDocumentationEvent;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+
+class DocumentationFilterCommandEventListener
+{
+    public const FILTER_OUT = 'out';
+    public const FILTER_IN = 'in';
+
+    public function __construct(private array $commandNames, private string $filter = self::FILTER_OUT)
+    {
+    }
+
+    #[AsEventListener]
+    public function onGenerateDocumentationEvent(GenerateDocumentationEvent $event): void
+    {
+        $match = \in_array($event->getCommand()->getName(), $this->commandNames);
+
+        if ($match && self::FILTER_OUT === $this->filter) {
+            $event->ignore();
+
+            return;
+        }
+
+        if (!$match && self::FILTER_IN === $this->filter) {
+            $event->ignore();
+        }
+    }
+}
