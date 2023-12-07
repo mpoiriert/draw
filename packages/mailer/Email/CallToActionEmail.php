de749cd76ec15299f38f1e72276fc4fe4094f404
Martin Poirier Théorêt <mpoiriert@gmail.com>
2023-12-07 15:23:48 -0500

    [Mailer] default translations tokent for call to action template

--- packages/mailer/Email/CallToActionEmail.php
+++ packages/mailer/Email/CallToActionEmail.php
@@ -8,11 +8,15 @@ class CallToActionEmail extends TemplatedEmail
 {
     private ?string $callToActionLink = null;
 
+    public array $translationTokens = [];
+
     public function getContext(): array
     {
         $context = parent::getContext();
 
         $context['call_to_action_link'] = $this->callToActionLink;
+
+        $context['translation_tokens'] = $this->translationTokens;
 
         return $context;
     }
