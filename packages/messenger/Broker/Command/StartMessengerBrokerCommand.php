396213faaf16eb2a1b4a99e77574f15c8052ef54
Adrian Dumitrache <dumitrache.ady91@gmail.com>
2024-03-19 12:10:05 +0200

    [235] Auto calculate the amount of concurrent processes for the broker.

--- packages/messenger/Broker/Command/StartMessengerBrokerCommand.php
+++ packages/messenger/Broker/Command/StartMessengerBrokerCommand.php
@@ -3,6 +3,7 @@
 namespace Draw\Component\Messenger\Broker\Command;
 
 use Draw\Component\Messenger\Broker\Broker;
+use Draw\Component\Messenger\Counter\CpuCounter;
 use Draw\Contracts\Process\ProcessFactoryInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\InvalidOptionException;
@@ -14,10 +15,13 @@ use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 class StartMessengerBrokerCommand extends Command
 {
+    private const OPTION_VALUE_CONCURRENT_AUTO = 'auto';
+
     public function __construct(
         private string $consolePath,
         private ProcessFactoryInterface $processFactory,
-        private EventDispatcherInterface $eventDispatcher
+        private EventDispatcherInterface $eventDispatcher,
+        private CpuCounter $cpuCounter,
     ) {
         parent::__construct();
     }
@@ -38,7 +42,30 @@ class StartMessengerBrokerCommand extends Command
                 'concurrent',
                 null,
                 InputOption::VALUE_REQUIRED,
-                'The number of concurrent consumer you want to run',
+                sprintf(
+                    'The number of concurrent consumers you want to run; use "%s" to use the auto calculation of CPU',
+                    self::OPTION_VALUE_CONCURRENT_AUTO
+                ),
+                1
+            )
+            ->addOption(
+                'processes-per-core',
+                null,
+                InputOption::VALUE_REQUIRED,
+                sprintf(
+                    'The number of processes per CPU (used only if "concurrent" is set to "%s")',
+                    self::OPTION_VALUE_CONCURRENT_AUTO
+                ),
+                1
+            )
+            ->addOption(
+                'minimum-processes',
+                null,
+                InputOption::VALUE_REQUIRED,
+                sprintf(
+                    'Minimum number of processes (used only if "concurrent" is set to "%s")',
+                    self::OPTION_VALUE_CONCURRENT_AUTO
+                ),
                 1
             )
             ->addOption(
@@ -54,10 +81,7 @@ class StartMessengerBrokerCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
-        $concurrent = (int) $input->getOption('concurrent');
-        if ($concurrent <= 0) {
-            throw new InvalidOptionException('Concurrent value ['.$concurrent.'] is invalid. Must be 1 or greater');
-        }
+        $concurrent = $this->getConcurrent($input);
 
         $timeout = (int) $input->getOption('timeout');
         if ($timeout < 0) {
@@ -78,5 +102,44 @@ class StartMessengerBrokerCommand extends Command
     protected function createBroker(string $context): Broker
     {
         return new Broker($context, $this->consolePath, $this->processFactory, $this->eventDispatcher);
+    }
+
+    private function getConcurrent(InputInterface $input): int
+    {
+        $concurrent = $input->getOption('concurrent');
+        if (self::OPTION_VALUE_CONCURRENT_AUTO === $concurrent) {
+            return $this->calculateAutoConcurrent($input);
+        }
+
+        $concurrent = (int) $concurrent;
+        if ($concurrent <= 0) {
+            throw new InvalidOptionException(sprintf(
+                'Concurrent value [%d] is invalid. Must be 1 or greater',
+                $concurrent
+            ));
+        }
+
+        return $concurrent;
+    }
+
+    private function calculateAutoConcurrent(InputInterface $input): int
+    {
+        $processesPerCore = (float) $input->getOption('processes-per-core');
+        if ($processesPerCore <= 0) {
+            throw new InvalidOptionException(sprintf(
+                'Processes per core value [%f] is invalid. Must be greater than 0',
+                $processesPerCore
+            ));
+        }
+
+        $minProcesses = (int) $input->getOption('minimum-processes');
+        if ($minProcesses <= 0) {
+            throw new InvalidOptionException(sprintf(
+                'Minimum processes value [%d] is invalid. Must be greater than 0',
+                $minProcesses
+            ));
+        }
+
+        return max([$minProcesses, (int) round($processesPerCore * $this->cpuCounter->count())]);
     }
 }
