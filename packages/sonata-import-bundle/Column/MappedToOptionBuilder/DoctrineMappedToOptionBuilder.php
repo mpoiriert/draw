df9359ab13e191d5336d11a29b963d0d66ff6f17
Martin Poirier Théorêt <mpoiriert@gmail.com>
2024-04-23 15:26:19 -0400

    [SonataImportBundle] support doctrine toOne relation update (#253)

--- packages/sonata-import-bundle/Column/MappedToOptionBuilder/DoctrineMappedToOptionBuilder.php
+++ packages/sonata-import-bundle/Column/MappedToOptionBuilder/DoctrineMappedToOptionBuilder.php
@@ -6,12 +6,46 @@ use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\Mapping\ClassMetadataInfo;
 use Doctrine\Persistence\ManagerRegistry;
 use Draw\Bundle\SonataImportBundle\Entity\Column;
+use Draw\Bundle\SonataImportBundle\Event\AttributeImportEvent;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 
 class DoctrineMappedToOptionBuilder implements MappedToOptionBuilderInterface
 {
     public function __construct(
         private ManagerRegistry $managerRegistry,
     ) {
+    }
+
+    #[AsEventListener]
+    public function handleAttributeImportEvent(AttributeImportEvent $event): void
+    {
+        $column = $event->getColumn();
+
+        if (!str_contains($column->getMappedTo(), '.')) {
+            return;
+        }
+
+        $class = $column->getImport()->getEntityClass();
+
+        $metadata = $this->managerRegistry->getManagerForClass($class)->getClassMetadata($class);
+
+        [$relation, $field] = explode('.', $column->getMappedTo());
+
+        if (!isset($metadata->associationMappings[$relation])) {
+            return;
+        }
+
+        $targetEntityClass = $metadata->associationMappings[$relation]['targetEntity'];
+
+        $targetEntity = $this->managerRegistry->getRepository($targetEntityClass)->findOneBy([$field => $event->getValue()]);
+
+        if (null === $targetEntity) {
+            return;
+        }
+
+        $event->getEntity()->{'set'.ucfirst($relation)}($targetEntity);
+
+        $event->stopPropagation();
     }
 
     public function getOptions(Column $column, array $options): array
