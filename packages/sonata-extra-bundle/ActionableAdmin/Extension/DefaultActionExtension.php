7f647ddea62ead6e07af66019894c790862d069a
Martin Poirier Théorêt <mpoiriert@gmail.com>
2024-08-10 09:19:24 -0400

    [SonataExtraBundle] DefaultActionExtension registered manually

--- packages/sonata-extra-bundle/ActionableAdmin/Extension/DefaultActionExtension.php
+++ packages/sonata-extra-bundle/ActionableAdmin/Extension/DefaultActionExtension.php
@@ -4,18 +4,37 @@ namespace Draw\Bundle\SonataExtraBundle\ActionableAdmin\Extension;
 
 use Draw\Bundle\SonataExtraBundle\ActionableAdmin\AdminAction\DeleteAdminAction;
 use Sonata\AdminBundle\Admin\AbstractAdminExtension;
-use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 
-#[AutoconfigureTag('sonata.admin.extension', attributes: ['global' => true])]
 class DefaultActionExtension extends AbstractAdminExtension implements ActionableAdminExtensionInterface
 {
+    private const SUPPORTED_ACTIONS = [
+        'delete' => DeleteAdminAction::class,
+    ];
+
+    private array $actions;
+
+    public function __construct(
+        array $actions = []
+    ) {
+        $this->actions = array_values(
+            array_intersect(
+                $actions,
+                array_keys(self::SUPPORTED_ACTIONS)
+            )
+        );
+    }
+
     public function getActions(array $actions): array
     {
-        if (\array_key_exists('delete', $actions)) {
-            return $actions;
-        }
+        foreach ($this->actions as $action) {
+            if (\array_key_exists($action, $actions)) {
+                continue;
+            }
 
-        $actions['delete'] = new DeleteAdminAction();
+            $actionClass = self::SUPPORTED_ACTIONS[$action];
+
+            $actions[$action] = new $actionClass();
+        }
 
         return $actions;
     }
