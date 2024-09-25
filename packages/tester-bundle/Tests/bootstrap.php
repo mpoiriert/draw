c38b5e0f693eae797741cd43695ca825b3d09ec0
Martin Poirier Théorêt <mpoiriert@gmail.com>
2024-09-25 15:42:57 -0400

    fix test bundle automation test

---
+++ packages/tester-bundle/Tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\ErrorHandler\ErrorHandler;
+
+// Hack for PHPUnit 11.0.0
+// see https://github.com/symfony/symfony/issues/53812
+// can probably be removed on next Symfony 6.4.x release
+$phpunitsHandler = set_error_handler(static fn () => null);
+restore_exception_handler();
+ErrorHandler::register(null);
+set_error_handler($phpunitsHandler);
