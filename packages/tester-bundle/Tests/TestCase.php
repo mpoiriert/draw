c5aaf427e9e5bca2d4be567a3f7984f8f44b5e39
Martin Poirier Théorêt <mpoiriert@gmail.com>
2024-09-28 19:51:26 -0400

    [Tester/TesterBundle] Remove Tester and TesterBundle Http component

--- packages/tester-bundle/Tests/TestCase.php
+++ packages/tester-bundle/Tests/TestCase.php
@@ -2,25 +2,15 @@
 
 namespace Draw\Bundle\TesterBundle\Tests;
 
-use Draw\Bundle\TesterBundle\Http\BrowserFactoryInterface;
-use Draw\Bundle\TesterBundle\Http\HttpTesterTrait;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Symfony\Component\BrowserKit\AbstractBrowser;
 
-class TestCase extends KernelTestCase implements BrowserFactoryInterface
+class TestCase extends KernelTestCase
 {
-    use HttpTesterTrait;
-
     // For symfony 4.x
     protected static $class = AppKernel::class;
 
     protected static function getKernelClass(): string
     {
         return AppKernel::class;
-    }
-
-    public function createBrowser(): AbstractBrowser
-    {
-        return static::bootKernel()->getContainer()->get('test.client');
     }
 }
--- tests/Controller/Security/UserRequestInterceptedSubscriberTest.php
+++ tests/Controller/Security/UserRequestInterceptedSubscriberTest.php
@@ -2,8 +2,9 @@
 
 namespace App\Tests\Controller\Security;
 
-use App\Tests\AuthenticatorTestTrait;
+use App\Entity\User;
 use Draw\Bundle\TesterBundle\PHPUnit\Extension\SetUpAutowire\AutowireClient;
+use Draw\Bundle\TesterBundle\PHPUnit\Extension\SetUpAutowire\AutowireEntity;
 use Draw\Bundle\TesterBundle\WebTestCase;
 use Draw\Component\Tester\PHPUnit\Extension\SetUpAutowire\AutowiredInterface;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
@@ -11,8 +12,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class UserRequestInterceptedSubscriberTest extends WebTestCase implements AutowiredInterface
 {
-    use AuthenticatorTestTrait;
-
     #[AutowireClient(
         server: [
             'Content-Type' => 'application/json',
@@ -21,22 +20,18 @@ class UserRequestInterceptedSubscriberTest extends WebTestCase implements Autowi
     )]
     private KernelBrowser $client;
 
+    #[AutowireEntity(['email' => '2fa-admin@example.com'])]
+    private User $user;
+
     public function testRedirectedErrorMessage(): void
     {
-        static::setAuthorizationHeader($this->client, '2fa-admin@example.com');
+        $this->client->loginUser($this->user);
 
         $this->client->request('get', '/api/users');
 
         static::assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
 
-        $dataTester = static::getJsonResponseDataTester();
-
-        static::assertSame(
-            'User request intercepted: 2fa_need_enabling',
-            $dataTester->getData('message')
-        );
-
-        $dataTester
+        static::getJsonResponseDataTester()
             ->path('message')
             ->assertSame('User request intercepted: 2fa_need_enabling');
     }
