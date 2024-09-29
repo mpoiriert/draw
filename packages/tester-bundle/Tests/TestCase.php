25b48b649c8f58b70365c8d49b9de4bcc592a14d
Martin Poirier Théorêt <mpoiriert@gmail.com>
2024-09-28 20:44:13 -0400

    [Internal] Refactor automation test

--- packages/tester-bundle/Tests/TestCase.php
+++ packages/tester-bundle/Tests/TestCase.php
@@ -6,9 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class TestCase extends KernelTestCase
 {
-    // For symfony 4.x
-    protected static $class = AppKernel::class;
-
     protected static function getKernelClass(): string
     {
         return AppKernel::class;
