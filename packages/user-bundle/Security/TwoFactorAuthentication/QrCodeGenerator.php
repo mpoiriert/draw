3a33e42679ecaebfdc371214c6217c39a92c15bb
Adrian Dumitrache <dumitrache.ady91@gmail.com>
2024-11-25 20:00:57 +0200

    [UserBundle] Upgrade endroid/qr-code package (#306)

--- packages/user-bundle/Security/TwoFactorAuthentication/QrCodeGenerator.php
+++ packages/user-bundle/Security/TwoFactorAuthentication/QrCodeGenerator.php
@@ -3,9 +3,7 @@
 namespace Draw\Bundle\UserBundle\Security\TwoFactorAuthentication;
 
 use Endroid\QrCode\Builder\Builder;
-use Endroid\QrCode\Encoding\Encoding;
 use Endroid\QrCode\ErrorCorrectionLevel;
-use Endroid\QrCode\RoundBlockSizeMode;
 use Endroid\QrCode\Writer\Result\ResultInterface;
 use Endroid\QrCode\Writer\SvgWriter;
 
@@ -13,16 +11,12 @@ class QrCodeGenerator
 {
     public function getTotpQrCode(string $qrCodeContent): ResultInterface
     {
-        return Builder::create()
-            ->writer(new SvgWriter())
-            ->writerOptions([])
-            ->data($qrCodeContent)
-            ->encoding(new Encoding('UTF-8'))
-            ->errorCorrectionLevel(ErrorCorrectionLevel::High)
-            ->size(200)
-            ->margin(0)
-            ->roundBlockSizeMode(RoundBlockSizeMode::Margin)
-            ->build()
-        ;
+        return (new Builder(
+            new SvgWriter(),
+            data: $qrCodeContent,
+            errorCorrectionLevel: ErrorCorrectionLevel::High,
+            size: 200,
+            margin: 0,
+        ))->build();
     }
 }
