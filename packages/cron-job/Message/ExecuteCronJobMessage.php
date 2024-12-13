4a1b6d2c49b73192b50ce5d105abcf3d0b18e332
Martin Poirier Théorêt <mpoiriert@gmail.com>
2024-12-12 23:21:09 -0500

    [CronJob] Integrate DoctrineEnvelopeEntityReference custom exception

--- packages/cron-job/Message/ExecuteCronJobMessage.php
+++ packages/cron-job/Message/ExecuteCronJobMessage.php
@@ -6,14 +6,15 @@ namespace Draw\Component\CronJob\Message;
 
 use Draw\Component\CronJob\Entity\CronJobExecution;
 use Draw\Component\Messenger\AutoStamp\Message\StampingAwareInterface;
+use Draw\Component\Messenger\DoctrineEnvelopeEntityReference\Exception\ObjectNotFoundException;
 use Draw\Component\Messenger\DoctrineEnvelopeEntityReference\Message\DoctrineReferenceAwareInterface;
+use Draw\Component\Messenger\DoctrineEnvelopeEntityReference\Stamp\PropertyReferenceStamp;
 use Symfony\Component\Messenger\Bridge\Amqp\Transport\AmqpStamp;
 use Symfony\Component\Messenger\Envelope;
-use Symfony\Component\Messenger\Exception\UnrecoverableMessageHandlingException;
 
 class ExecuteCronJobMessage implements DoctrineReferenceAwareInterface, StampingAwareInterface
 {
-    private ?CronJobExecution $execution;
+    private PropertyReferenceStamp|CronJobExecution|null $execution;
 
     public function __construct(CronJobExecution $execution)
     {
@@ -22,8 +23,8 @@ class ExecuteCronJobMessage implements DoctrineReferenceAwareInterface, Stamping
 
     public function getExecution(): CronJobExecution
     {
-        if (null === $this->execution) {
-            throw new UnrecoverableMessageHandlingException('CronJobExecution is not set.');
+        if (!$this->execution instanceof CronJobExecution) {
+            throw new ObjectNotFoundException(CronJobExecution::class, $this->execution);
         }
 
         return $this->execution;
