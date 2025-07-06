ca725b270150cda01925683c53181efdcb443b0e
Martin Poirier Théorêt <mpoiriert@gmail.com>
2025-07-06 13:54:26 -0400

    [TesterBundle/SetUpAutowire] Add AutowireServiceMock automation test

--- tests/TesterBundle/PHPUnit/Extension/SetUpAutoWire/AutowireServiceMockTest.php
+++ tests/TesterBundle/PHPUnit/Extension/SetUpAutoWire/AutowireServiceMockTest.php
@@ -4,6 +4,7 @@ namespace App\Tests\TesterBundle\PHPUnit\Extension\SetUpAutoWire;
 
 use App\Entity\User;
 use Draw\Bundle\TesterBundle\PHPUnit\Extension\SetUpAutowire\AutowireClient;
+use Draw\Bundle\TesterBundle\PHPUnit\Extension\SetUpAutowire\AutowireService;
 use Draw\Bundle\TesterBundle\PHPUnit\Extension\SetUpAutowire\AutowireServiceMock;
 use Draw\Bundle\TesterBundle\WebTestCase;
 use Draw\Component\Tester\PHPUnit\Extension\SetUpAutowire\AutowiredInterface;
@@ -20,11 +21,22 @@ class AutowireServiceMockTest extends WebTestCase implements AutowiredInterface
     private KernelBrowser $client;
 
     #[AutowireServiceMock]
-    private EntityHandler&MockObject $entityHandler;
+    private EntityHandler&MockObject $entityHandlerMock;
+
+    #[AutowireService]
+    private EntityHandler $entityHandler;
+
+    public function testInstanceOfEntityHandler(): void
+    {
+        static::assertSame(
+            $this->entityHandlerMock,
+            $this->entityHandler
+        );
+    }
 
     public function testUsersAction(): void
     {
-        $this->entityHandler
+        $this->entityHandlerMock
             ->expects(static::once())
             ->method('findAll')
             ->with(User::class)
